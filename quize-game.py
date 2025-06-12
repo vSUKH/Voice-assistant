1fcc1a62774d46aa303abd356b6d8a18689d5edc
Sukhmander Singh <142488907+vSUKH@users.noreply.github.com>
2025-06-12 09:34:50 +0530

    Add files via upload

---
+++ quize-game.py
@@ -0,0 +1,484 @@
+welcome = input("Welcome to my quiz!")
+playing = input("Shall we start? ")
+
+if playing != "yes":
+    quit()
+score = 0
+
+answer = input('''Which one of the following rivers originates in Brahmagiri range of Western Ghats?
+(a) Pennar
+(b) Cauvery
+(c) Krishna
+(d) Tapti\n ''')
+
+if answer == "b":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+answer = input('''\n The country that has the highest in Uranium Production?
+(a) China
+(b) Kazakstan
+(c) Australia
+(d) France
+\n ''')
+
+if answer == "b":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+answer = input('''Which HTML5 attribute is used to enable geolocation services in a web page?
+
+a) geo-enable
+b) location-enable
+c) geolocation
+d) positioning
+\n ''')
+
+if answer == "c":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+answer = input('''What is the date of Summer Solstice?
+
+a) 21 June
+b) 5 August
+c) 22 December
+d) 10 March
+\n ''')
+
+if answer == "a":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+
+answer = input('''How many tiers are there in a Panchayati Raj structure ?
+a)Three 
+
+b)Four 
+
+c)Five 
+
+d)Two 
+\n ''')
+
+if answer == "a":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+
+answer = input('''What is the smallest ocean in the world?
+
+a) Atlantic Ocean
+b) Indian Ocean
+c) Arctic Ocean
+d) Pacific Ocean
+\n ''')
+
+if answer == "c":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+answer = input('''Which river is known as the "Ganga of the South"?
+
+a) Yamuna
+b) Godavari
+c) Brahmaputra
+d) Cauvery
+\n ''')
+
+if answer == "b":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+answer = input('''Who is known as the "Father of the Nation" in India?
+
+a) Jawaharlal Nehru
+b) Sardar Patel
+c) Mahatma Gandhi
+d) Subhas Chandra Bose
+\n ''')
+
+if answer == "c":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+answer = input('''What is the capital city of Maharashtra?
+
+a) Mumbai
+b) Pune
+c) Nagpur
+d) Aurangabad
+\n ''')
+
+if answer == "a":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+
+answer = input('''What is the national flower of India?
+
+a) Rose
+b) Lotus
+c) Marigold
+d) Lily
+\n ''')
+
+if answer == "b":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+answer = input('''Which HTML tag is used to define an unordered list?
+
+a) <ol>
+b) <li>
+c) <ul>
+d) <dl>
+\n ''')
+
+if answer == "c":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+answer = input('''What is the purpose of the <head> element in HTML?
+
+a) It defines the main content of the document.
+b) It contains metadata about the HTML document.
+c) It creates a hyperlink in the document.
+d) It defines a header for the document.
+\n ''')
+
+if answer == "d":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is d")
+
+
+
+answer = input('''In HTML, which tag is used to create a hyperlink?
+
+a) <link>
+b) <a>
+c) <h1>
+d) <p>
+\n ''')
+
+if answer == "b":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+answer = input('''What is the purpose of the <meta charset="UTF-8"> tag in HTML?
+
+a) It sets the background color of the page.
+b) It defines the character encoding for the document.
+c) It specifies the font style for the text.
+d) It creates a metadata description for search engines.
+\n ''')
+
+if answer == "b":
+    print("correct") 
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+answer = input('''\n\nWhich HTML5 element is used to embed external applications or interactive content?
+
+a) <iframe>
+b) <embed>
+c) <object>
+d) <applet>
+\n ''')
+
+if answer == "a":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+
+
+
+
+
+
+
+
+
+answer = input(''' The only private sector refinery set up by Reliance Petroleum Ltd. is located at
+A. Guwahati
+B. Jamnagar
+C. Mumbai
+D. Chennai\n''')
+
+if answer == "b":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+answer = input('''The only state in India that produces saffron is
+A. Assam
+B. Himachal Pradesh
+C. Jammu and Kashmir
+D. Meghalaya\n''')
+
+if answer == "c":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+answer = input('''The Barak River is a famous river of:
+A. The Western Ghat
+B. Eastern Ghat
+C. Satpura hills
+D. Manipur hills\n''')
+
+if answer == "d":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is d")
+answer = input('''Three important rivers of the Indian subcontinent have their sources near the 
+Mansarover Lake in the Great Himalayas. These rivers are
+A. Indus Jhelum and Sutlej
+B. Brahmaputra Sutlej and Yamuna
+C. Brahmaputra Indus and Sutlej
+D. Jhelum Sutlej and Yamuna\n''')
+
+if answer == "b":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+answer = input('''The zonal soil type of peninsular India belongs to
+A. red soils
+B. yellow soils
+C. black soils
+D. older alluvium\n
+''')
+
+if answer == "c":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+answer = input('''The largest river of all the west flowing rivers of the peninsular India is:
+A. Tapti
+B. Kaveri
+C. Krishna
+D. Narmada\n''')
+
+if answer == "a":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+answer = input('''The world's largest producer of natural gas is:
+A. America
+B. England
+C. Russia
+D. India\n''')
+
+if answer == "c":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+
+
+
+answer = input(''' Any wind blowing down the slope of a mountain is called:
+A. Anabatic Wind
+B. Katabatic Wind
+C. Down Falling Wind
+D. Slope Wind\n''')
+
+if answer == "b":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+
+
+
+answer = input('''The zone of low atmospheric pressure and ascending air located at or near the 
+equator is called:
+A. Inter-Tropical Convergence Zone (ITCZ)
+B. Outer-Tropical Convergence Zone
+C. Intera-Tropical Convergence Zone
+D. None of Above\n''')
+
+if answer == "a":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+
+
+
+
+answer = input('''Sandstone, conglomerate, limestone, shalepotash, loess, geyserites, chalk, 
+coal, chert, halite are the examples of:
+A. Igneous rock
+B. Metamorphic
+C. Sedimentary Rocks
+D. None of Above\n
+''')
+
+if answer == "c":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+
+
+
+answer = input('''The industrial region of India which has the predominance of metal industries 
+is:
+A. Penisular Region
+B. Chotanagpur Region
+C. Delta Region
+D. Brahmputra Region\n''')
+
+if answer == "b":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+
+
+
+answer = input('''Singareni Coal fields is located in :
+A. Narmada Valley
+B. Karishna Valley
+C. Godavari Valley
+D. Kaveri Valley\n
+''')
+
+if answer == "c":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+
+
+
+answer = input('''Tilaiya Dam, Konar dam, Maithon Dam and Panchet Hill Dam have been 
+constructed under:
+A. The Narmada Valley Project
+B. The Damodar Valley Project
+C. The Krishna Valley Project
+D. The Mahanadi Project\n''')
+
+if answer == "b":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is b")
+
+
+
+
+
+
+answer = input('''The radiant energy received by the earth and its atmosphere from the sun is 
+called:
+A. Insolation
+B. Isotherm
+C. Insoenergy
+D. Albedo\n''')
+
+if answer == "a":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is a")
+
+
+
+
+
+
+answer = input('''Which river is known as Sorrow of Bihar?
+A. Ganga River
+B. Gandak River
+C. Kosi River
+D. Son River\n''')
+
+if answer == "c":
+    print("correct")  
+    score += 1
+else:
+    print("Wrong!! Right answer is c")
+
+
+
+
+print("Yoy got  " + str(score) + "   score")
+
+
+
