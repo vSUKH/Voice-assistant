e58399f864e085c8653d9f827045595c407d587b
Sukhmander Singh <142488907+vSUKH@users.noreply.github.com>
2025-06-24 10:34:07 +0530

    Delete firstdays.py

--- firstdays.py
+++
@@ -1,8 +0,0 @@
-a = print('hello mam')  #single
-b  = print('''whats'up ?''') #single tripal
-c = print ("what's going on ? \nAre you ok ?")#double 
-
-
-d = print("hello " "vishal")
-d.upper()
-print(d)
